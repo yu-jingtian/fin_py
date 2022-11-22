16ae5545df3f4eb0c01d126eef1f97ee91d038e5
timyu723 <yujingtian0723@gmail.com>
2022-11-21 23:57:52 -0800

    exclude low volume

--- EFT_CN/eff_front_CN_ETF.py
+++ EFT_CN/eff_front_CN_ETF.py
@@ -246,6 +246,20 @@ def allocSumFun(allocation):
     alloc_Vol = alloc_Vol.loc[:, (alloc_Vol != 0).any(axis=0)]
     return(alloc_SR, alloc_Vol)
 
+def sharesFun(alloc_SR, endDate, total = 10000):
+    stocks = alloc_SR.columns.values[1:].tolist()
+    stockData = pdr.get_data_yahoo(stocks, start=endDate, end=endDate)
+    price = stockData['Close'].values
+    
+    alloc_money = (alloc_SR.values[:,1:]/100*total).astype(int)
+    
+    shares = np.round(alloc_money/price)
+    
+    shares = pd.DataFrame(shares, columns = stocks)
+    shares.insert(0, "Portfolio Shares", ["Max SR"], True)
+    
+    return(shares)
+
 def EFPlot(meanReturns, covMatrix, rslt, startDate, endDate, country = "US"):
     """Return a graph ploting the min vol, max sr and efficient frontier"""
     maxSR_returns = rslt[0] 
@@ -313,29 +327,9 @@ days = 30
 endDate = dt.datetime.today() - dt.timedelta(days=1) # start date: yesterday
 startDate = endDate - dt.timedelta(days=days)
 
-fund_SS = pd.read_csv("fundList_SS.csv", header=None)
-fund_SZ = pd.read_csv("fundList_SZ.csv", header=None)
+fundList = pd.read_csv("fundVolumeFilter.csv", header=None)
 
-fundList_SS = [str(fund) for fund in fund_SS.iloc[:, 0]]
-fundList_SZ = [str(fund) for fund in fund_SZ.iloc[:, 0]]
-
-stocks_SS = [stock+'.SS' for stock in fundList_SS]
-stocks_SZ = [stock+'.SZ' for stock in fundList_SZ]
-
-stocks_all = stocks_SS + stocks_SZ
-
-'''
-Remove funds with no data on yahoo finance
-'''
-rm_list = pd.read_csv('list_nan.txt', sep=' ', header=None, )
-rm_list = rm_list.drop(range(1, 168, 2))
-stock_rm = [str(fund)[1:10] for fund in rm_list.iloc[:, 6]]
-
-
-# remove NaN stocks
-stocks = [stock for stock in stocks_all if stock not in stock_rm]
-
-
+stocks = [str(fund) for fund in fundList.iloc[:, 0]]
 
 print("Load Data...")
 meanReturns, covMatrix = getData(stocks=stocks, start=startDate, end=endDate)
@@ -349,6 +343,8 @@ print("Optimization done.")
 print("Result Presentation...")
 info, returnVol, allocation = rsltOutput(rslt, startDate, endDate)
 alloc_SR, alloc_Vol = allocSumFun(allocation)
+shares_SR = sharesFun(alloc_SR, endDate, total = 10000)
+
 
 print(info.to_markdown(index=False))
 print(returnVol.to_markdown(index=False))
---
+++ EFT_CN/eff_front_CN_ETF_Volume.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 21 22:33:18 2022
+
+@author: timyu
+"""
+
+import numpy as np
+import datetime as dt
+import pandas as pd
+import scipy.optimize as sc
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import time
+
+from pandas_datareader import data as pdr
+
+def getData_Volume(stocks, start, end):
+    stockData = pdr.get_data_yahoo(stocks, start=start, end=end)
+    volumeData = stockData['Volume']
+    meanVolume = volumeData.mean(skipna=True)
+    
+    # filter funds with Volume >= 10,000,000
+    meanVolume = meanVolume[meanVolume >= 1e7]
+    
+    return meanVolume.index.tolist()
+
+# 252 trading days per year 
+days = 30
+endDate = dt.datetime.today() - dt.timedelta(days=1) # start date: yesterday
+startDate = endDate - dt.timedelta(days=days)
+
+fund_SS = pd.read_csv("fundList_SS.csv", header=None)
+fund_SZ = pd.read_csv("fundList_SZ.csv", header=None)
+
+fundList_SS = [str(fund) for fund in fund_SS.iloc[:, 0]]
+fundList_SZ = [str(fund) for fund in fund_SZ.iloc[:, 0]]
+
+stocks_SS = [stock+'.SS' for stock in fundList_SS]
+stocks_SZ = [stock+'.SZ' for stock in fundList_SZ]
+
+stocks_all = stocks_SS + stocks_SZ
+
+'''
+Remove funds with no data on yahoo finance
+'''
+rm_list = pd.read_csv('list_nan.txt', sep=' ', header=None, )
+rm_list = rm_list.drop(range(1, 168, 2))
+stock_rm = [str(fund)[1:10] for fund in rm_list.iloc[:, 6]]
+
+
+# remove NaN stocks
+stocks = [stock for stock in stocks_all if stock not in stock_rm]
+
+print("Load Data...")
+pd.DataFrame(getData_Volume(stocks=stocks, start=startDate, end=endDate)).to_csv("fundVolumeFilter.csv", header=False, index=False)
+print("Data Loaded.")
